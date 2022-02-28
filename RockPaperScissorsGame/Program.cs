227df8e08c78d9be7508280007200e26a1de43ae
Adam <Adam@DESKTOP-4EFD29B>
2022-02-28 14:39:55 -0500

    InitialCommit

---
+++ RockPaperScissorsGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using RockPaperScissorGameDemo;
+Class1 _repo = new Class1();
+
+Console.WriteLine("WELCOME TO THE WORLD OF ROCK & PAPER AND SCISSORS!!!!!\n");
+Console.WriteLine("Press any key to continue.............................");
+Console.ReadKey();
+Console.Clear();
+Console.Write("How would you like to appear on the leaderboard!!!!");
+string userName = Console.ReadLine();
+Console.WriteLine("I guess that is a cool name " + userName);
+Console.WriteLine("If you are ready to begin hit any key...");
+Console.ReadKey();
+
+
+Console.WriteLine("Would you like to Choose Either ROCK, Paper or Scissors?\n");
+Console.WriteLine("Choose 1 for Rock\n");
+Console.WriteLine("Choose 2 for Paper\n");
+Console.WriteLine("Choose 3 for Scissors");
+string userInput = Console.ReadLine();
+string userChoice = _repo.GameChoice(userInput);
+string computerInput = _repo.RandomComputerNumber();
+string computerChoice = _repo.GameChoice(computerInput);
+
+_repo.Outcome(userChoice, computerChoice);
---
+++ RockPaperScissorsRepo/Class1.cs
@@ -0,0 +1,83 @@
+﻿namespace RockPaperScissorGameDemo{
+
+
+public class Class1
+{
+    public string RandomComputerNumber(){
+        Random _random = new Random();
+        int randomChoice = _random.Next(1,4);
+        string computerChoice = randomChoice.ToString();
+        return computerChoice;
+    }
+    public string GameChoice(string input)
+    {
+        switch(input){
+
+        case "1":
+        string choice = "rock";
+        return choice;
+        case "2":
+        string choiceTwo = "paper";
+        return choiceTwo;
+        case "3":
+        string choiceThree = "scissors";
+        return choiceThree;
+        default:
+        Console.WriteLine("Not a Valid Response");
+        break;
+
+        }
+        return null;
+
+    }
+
+
+
+public void Outcome(string playerChoice, string computerChoice)
+{
+if(playerChoice == "rock" && computerChoice == "paper")
+{
+    YouLose();
+}
+else if(playerChoice == "paper" && computerChoice == "scissors")
+{
+     YouLose();
+}
+
+else if(playerChoice == "scissors" && computerChoice == "rock")
+{
+   YouLose();
+}
+else if(playerChoice == "rock" && computerChoice == "scissors")
+{
+    YouWin();
+}
+else if(playerChoice == "paper" && computerChoice == "rock")
+{
+     YouWin();
+}
+
+else if(playerChoice == "scissors" && computerChoice == "paper")
+{
+    YouWin();
+}
+else
+{
+    Console.WriteLine("NOBODY WINS");
+}
+
+}
+
+
+public void YouWin()
+{
+    Console.WriteLine("YouWin");
+}
+
+public void YouLose()
+{
+    Console.WriteLine("YouLose");
+}
+
+}
+}
